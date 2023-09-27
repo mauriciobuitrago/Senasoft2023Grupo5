1f5390219dda3492784fddb71d30e8e683506964
Laura Arango <sofia.cadena444@gmail.com>
2023-09-27 14:59:59 -0500

    Se terminó login exitoso

---
+++ Automatización/ScreenplaySenaSoft/src/test/java/com/com/senasoft/stepsdefinitions/hooks.java
@@ -0,0 +1,22 @@
+package com.com.senasoft.stepsdefinitions;
+
+import cucumber.api.java.Before;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actors.Cast;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+
+public class hooks {
+    @Managed
+    WebDriver hisBrowser;
+
+    @Before
+    public void setUp()
+    {
+        OnStage.setTheStage(Cast.ofStandardActors());
+        OnStage.theActorCalled("User");
+        OnStage.theActorInTheSpotlight().can(BrowseTheWeb.with(hisBrowser));
+    }
+
+}
