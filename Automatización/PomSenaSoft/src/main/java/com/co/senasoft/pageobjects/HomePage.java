ae7e0f3520c025de0c4f876339d26208fe8a14b6
Laura Arango <sofia.cadena444@gmail.com>
2023-09-27 12:47:02 -0500

    Se agregó pageobjects

--- Automatización/PomSenaSoft/src/main/java/com/co/senasoft/pageobjects/HomePage.java
+++ Automatización/PomSenaSoft/src/main/java/com/co/senasoft/pageobjects/HomePage.java
@@ -1,2 +1,13 @@
-package com.co.senasoft.pageobjects;public class HomePage {
+package com.co.senasoft.pageobjects;
+
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
+
+@DefaultUrl("https://www.booking.com/")
+public class HomePage extends PageObject {
+    By BTN_LIST_LANGUAGE = By.xpath("//button[@data-testid='header-language-picker-trigger']");
+    By BTN_LANGUAGE = By.xpath("//div[@class = 'aca0ade214 ebac6e22e9 cd2e7d62b0']/div[1]//ul[2]/li[1]/button");
+    By TEXT_DEUTSCH = By.xpath("//p[contains(text(),'Finden Sie Angebote für Hotels, Ferienunterkünfte und vieles mehr')]");
 }
--- Automatización/PomSenaSoft/src/test/java/com/co/senasoft/stepdefinitions/LanguageStepDefinition.java
+++ Automatización/PomSenaSoft/src/test/java/com/co/senasoft/stepdefinitions/LanguageStepDefinition.java
@@ -1,2 +1,26 @@
-package com.co.senasoft.stepdefinitions;public class LanguageStepDefinition {
+package com.co.senasoft.stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LanguageStepDefinition {
+    @Given("that the user in on the page")
+    public void thatTheUserInOnThePage() {
+
+    }
+    @When("he clicks on the language icon")
+    public void heClicksOnTheLanguageIcon() {
+
+    }
+    @And("clicks on Deutsch")
+    public void clicksOnDeutsch() {
+
+    }
+    @Then("He shoud see the page in Deutsch")
+    public void heShoudSeeThePageInDeutsch() {
+
+    }
+
 }
